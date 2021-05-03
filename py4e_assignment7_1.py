7a949cd2503f1c8f43fade9c99b0fa48a26c09db
iiSagarii <83596823+iiSagarii@users.noreply.github.com>
2021-05-03 20:42:20 +0530

    Update py4e_assignment7_1.py

--- py4e_assignment7_1.py
+++ py4e_assignment7_1.py
@@ -1,3 +1,6 @@
+# write a program that prompts for a file name, then opens that file and reads through the file,
+# Use the file words.txt to oproduce the output below
+
 user = input("Enter the file name:")
 s1 = '/Users/Sagar Pujari/PycharmProjects/'
 try :
