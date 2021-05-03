3f84afcdd4a88faeb0e96c8df4a1122aae62ca6f
iiSagarii <83596823+iiSagarii@users.noreply.github.com>
2021-05-03 20:46:41 +0530

    Update py4e_assignment_7_2.py

--- py4e_assignment_7_2.py
+++ py4e_assignment_7_2.py
@@ -1,3 +1,7 @@
+# Write a program that prompts for a file name, then opens that file and reads through the file, looking for lines of the form: programs
+# Count these lines and extract the floating-point values from each of the lines and compute the average of those values and produce an output as shown below.
+# Do not use the sum() function or a variable named sum in your solution.
+
 user = input("Enter file name : ")
 s1 = "/Users/Sagar Pujari/PycharmProjects/"
 s2 = ".txt"
