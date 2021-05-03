1b570cbfcbcde0f31c9bd5d98829ef6fc3959841
iiSagarii <83596823+iiSagarii@users.noreply.github.com>
2021-05-03 20:47:51 +0530

    Update py4e_assignment_8_4.py

--- py4e_assignment_8_4.py
+++ py4e_assignment_8_4.py
@@ -1,3 +1,10 @@
+# Open the file textfile.txt and read it line by line. 
+# For each line, split the line into a list of words using the split() method. 
+# The program should build a list of words. 
+# For each word on each line check to see if the word is already in the list and if not append it to the list. 
+# When the program completes, sort and print the resulting words in alphabetical order.
+
+
 file = open('/Users/Sagar Pujari/PycharmProjects/textfile.txt','r',encoding="utf-8")
 list1 = list()
 for line in file:
